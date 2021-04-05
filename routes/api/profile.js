e8894554aeb74e8977fde5dd74e9939e303861aa
Ananth <ananth.kumbla@gmail.com>
2021-04-05 22:18:25 +0530

    adding experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -155,4 +155,73 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).json("Something went wrong");
   }
 });
+
+// @route   PUT api/profile/experience
+// @desc  Add experience
+// @access Private
+
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("company", "Company is  required").not().isEmpty(),
+      check("from", "From date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = { title, company, location, from, to, current, description };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json("Something went wrong");
+    }
+  }
+);
+
+// @route   DELETE  api/profile/experience/exp_id
+// @desc  delete experience
+// @access Private
+
+router.delete(
+  "/experience/:exp_id",
+
+  auth,
+  async (req, res) => {
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      const removeIndex = profile.experience
+        .map((item) => item.id)
+        .indexOf(req.params.exp_id);
+      profile.experience.splice(removeIndex, 1);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json("Something went wrong");
+    }
+  }
+);
 module.exports = router;
