81f22f52a550af48a7a01917f1b567a7b302f3b1
Ananth <ananth.kumbla@gmail.com>
2021-04-05 23:09:02 +0530

    adding education and search users

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,8 @@ const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const { check, validationResult } = require("express-validator/check");
 const { restart } = require("nodemon");
+const request = require("request");
+const config = require("config");
 
 // @route GET api/profile/me
 // @desc Get current user profile
@@ -224,4 +226,113 @@ router.delete(
     }
   }
 );
+
+// adding education
+
+// @route   PUT api/profile/education
+// @desc  Add education
+// @access Private
+
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is required").not().isEmpty(),
+      check("degree", "Degree is  required").not().isEmpty(),
+      check("fieldofstudy", "Field of study is required").not().isEmpty(),
+      check("from", "From date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json("Something went wrong");
+    }
+  }
+);
+
+// @route   DELETE  api/profile/education/edu_id
+// @desc  delete education
+// @access Private
+
+router.delete(
+  "/education/:edu_id",
+
+  auth,
+  async (req, res) => {
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      const removeIndex = profile.education
+        .map((item) => item.id)
+        .indexOf(req.params.edu_id);
+      profile.education.splice(removeIndex, 1);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json("Something went wrong");
+    }
+  }
+);
+
+// @route   GET api/profile/github/:username
+// @desc  Get user respose from Github
+// @access Public
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+    request(options, (error, response, body) => {
+      if (error) console.log(error, "Error");
+
+      if (response.statusCode != 200)
+        return res.status(404).json({ msg: "No Github Profile found" });
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json("Something went wrong");
+  }
+});
 module.exports = router;
